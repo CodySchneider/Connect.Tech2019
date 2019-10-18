38c48ad46fe042a5988195409ffaaf42444d0340
Cody Schneider <cody.schneider@weather.com>
2019-10-18 09:36:21 -0400

    Add tablet and desktop diffs

--- src/organism/DailyForecast/DailyForecast.test.js
+++ src/organism/DailyForecast/DailyForecast.test.js
@@ -1,7 +1,27 @@
-test('DailyForecast: Image Diff', async () => {
+test('DailyForecast: Mobile Image Diff', async () => {
   // APIs from jest-puppeteer
   await page.goto('http://localhost:3000/iframe.html?id=organisms-modules-dailyforecast--default');
   await page.setViewport({width: 375, height: 667});
+  const image = await page.screenshot({fullPage: true});
+
+  // API from jest-image-snapshot
+  expect(image).toMatchImageSnapshot();
+});
+
+test('DailyForecast: Tablet Image Diff', async () => {
+  // APIs from jest-puppeteer
+  await page.goto('http://localhost:3000/iframe.html?id=organisms-modules-dailyforecast--default');
+  await page.setViewport({width: 600, height: 800});
+  const image = await page.screenshot({fullPage: true});
+
+  // API from jest-image-snapshot
+  expect(image).toMatchImageSnapshot();
+});
+
+test('DailyForecast: Desktop Image Diff', async () => {
+  // APIs from jest-puppeteer
+  await page.goto('http://localhost:3000/iframe.html?id=organisms-modules-dailyforecast--default');
+  await page.setViewport({width: 1024, height: 768});
   const image = await page.screenshot({fullPage: true});
 
   // API from jest-image-snapshot
